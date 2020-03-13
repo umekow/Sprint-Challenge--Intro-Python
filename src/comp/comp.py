d1a41ad12c2d40fcaaace90a8abad769953049eb
umekow <umekolashun@gmail.com>
2020-03-13 10:13:01 -0500

    first two problems in comp.py

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -24,13 +24,13 @@ humans = [
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [x for x in humans if x.name[0] == 'D' ]
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [x for x in humans if x.name[0].lower() == 'e']
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
